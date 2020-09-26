d10a9059c905f5b32ddab45318b340aa5a50d3f9
natenka <nataliya.samoylenko@gmail.com>
2020-09-26 14:51:12 +0000

    Examples 24_oop_inheritance done

--- examples/24_oop_inheritance/inheritance_example.py
+++ examples/24_oop_inheritance/inheritance_example.py
@@ -17,7 +17,8 @@ class ConnectSSH:
             username=username,
             password=password,
             look_for_keys=False,
-            allow_agent=False)
+            allow_agent=False,
+        )
 
         self._ssh = client.invoke_shell()
         time.sleep(1)
@@ -33,42 +34,41 @@ class ConnectSSH:
         self._ssh.close()
 
     def send_show_command(self, command):
-        self._ssh.send(command + '\n')
+        self._ssh.send(command + "\n")
         time.sleep(2)
-        result = self._ssh.recv(self._MAX_READ).decode('ascii')
+        result = self._ssh.recv(self._MAX_READ).decode("ascii")
         return result
 
     def send_config_commands(self, commands):
         if isinstance(commands, str):
             commands = [commands]
         for command in commands:
-            self._ssh.send(command + '\n')
+            self._ssh.send(command + "\n")
             time.sleep(0.5)
-        result = self._ssh.recv(self._MAX_READ).decode('ascii')
+        result = self._ssh.recv(self._MAX_READ).decode("ascii")
         return result
 
 
 class CiscoSSH(ConnectSSH):
-    def __init__(self, ip, username, password, enable_password,
-                 disable_paging=True):
+    def __init__(self, ip, username, password, enable_password, disable_paging=True):
         super().__init__(ip, username, password)
-        self._ssh.send('enable\n')
-        self._ssh.send(enable_password + '\n')
+        self._ssh.send("enable\n")
+        self._ssh.send(enable_password + "\n")
         if disable_paging:
-            self._ssh.send('terminal length 0\n')
+            self._ssh.send("terminal length 0\n")
         time.sleep(1)
         self._ssh.recv(self._MAX_READ)
 
     def config_mode(self):
-        self._ssh.send('conf t\n')
+        self._ssh.send("conf t\n")
         time.sleep(0.5)
-        result = self._ssh.recv(self._MAX_READ).decode('ascii')
+        result = self._ssh.recv(self._MAX_READ).decode("ascii")
         return result
 
     def exit_config_mode(self):
-        self._ssh.send('end\n')
+        self._ssh.send("end\n")
         time.sleep(0.5)
-        result = self._ssh.recv(self._MAX_READ).decode('ascii')
+        result = self._ssh.recv(self._MAX_READ).decode("ascii")
         return result
 
     def send_config_commands(self, commands):
@@ -77,7 +77,11 @@ class CiscoSSH(ConnectSSH):
         result += self.exit_config_mode()
         return result
 
+
 if __name__ == "__main__":
-    with CiscoSSH('192.168.100.1', 'cisco', 'cisco', 'cisco') as r1:
-        print(r1.send_config_commands(['interface loopback 33',
-                                       'ip address 3.3.3.3 255.255.255.255']))
+    with CiscoSSH("192.168.100.1", "cisco", "cisco", "cisco") as r1:
+        print(
+            r1.send_config_commands(
+                ["interface loopback 33", "ip address 3.3.3.3 255.255.255.255"]
+            )
+        )
--- examples/24_oop_inheritance/inheritance_example_netmiko.py
+++ examples/24_oop_inheritance/inheritance_example_netmiko.py
@@ -1,69 +1,70 @@
 from netmiko.cisco.cisco_ios import CiscoIosBase
 
 device_params = {
-    'device_type': 'cisco_ios',
-    'ip': '192.168.100.1',
-    'username': 'cisco',
-    'password': 'cisco',
-    'secret': 'cisco'
+    "device_type": "cisco_ios",
+    "ip": "192.168.100.1",
+    "username": "cisco",
+    "password": "cisco",
+    "secret": "cisco",
 }
 
-### Базовый пример наследования
+### Basic example of inheritance
+
 
 class MyNetmiko(CiscoIosBase):
     pass
 
 
 r1 = MyNetmiko(**device_params)
-print(r1.send_command('sh ip int br'))
+print(r1.send_command("sh ip int br"))
 
-### Добавление метода
+### Add child method
+
 
 class MyNetmiko(CiscoIosBase):
     def say_hello(self):
-        print('Hello from', self.ip)
+        print("Hello from", self.ip)
 
 
 r1 = MyNetmiko(**device_params)
-print(r1.send_command('sh ip int br'))
+print(r1.send_command("sh ip int br"))
 r1.say_hello()
 
-### Переопределение родительского метода
+### Rewrite parent method
 
 class MyNetmiko(CiscoIosBase):
     def send_command(self, command, *args, **kwargs):
         command_output = super().send_command(command, *args, **kwargs)
-        print('Вывод команды:')
+        print("Command output:")
         return command_output
 
     def say_hello(self):
-        print('Hello from', self.ip)
+        print("Hello from", self.ip)
 
 
 r1 = MyNetmiko(**device_params)
-print(r1.send_command('sh ip int br', strip_command=False))
+print(r1.send_command("sh ip int br", strip_command=False))
 
 
-### Добавление дополнительного функционала
+### Add error checking
 class ErrorInCommand(Exception):
     pass
 
+
 class MyNetmiko(CiscoIosBase):
     def send_command(self, command, *args, **kwargs):
         command_output = super().send_command(command, *args, **kwargs)
-        if 'Invalid input' in command_output:
-            raise ErrorInCommand('Ошибка в команде {}'.format(command))
+        if "Invalid input" in command_output:
+            raise ErrorInCommand("Error in command {}".format(command))
         return command_output
 
     def say_hello(self):
-        print('Hello from', self.ip)
+        print("Hello from", self.ip)
 
 
 r1 = MyNetmiko(**device_params)
 
 try:
-    r1.send_command('sh ip br', strip_command = False)
+    r1.send_command("sh ip br", strip_command=False)
 except ErrorInCommand as e:
     print(e)
-
-
