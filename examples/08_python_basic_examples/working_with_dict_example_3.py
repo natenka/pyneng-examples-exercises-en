2e92a63bb5135e0b03a8a168d4b7138d50bb6c26
natenka <nataliya.samoylenko@gmail.com>
2020-09-26 14:01:23 +0000

    Examples 08_python_basic_examples done

--- examples/08_python_basic_examples/working_with_dict_example_3.py
+++ examples/08_python_basic_examples/working_with_dict_example_3.py
@@ -1,16 +1,16 @@
 result = {}
 
-with open('sh_ip_interface.txt') as f:
+with open("sh_ip_interface.txt") as f:
     for line in f:
-        if 'line protocol' in line:
+        if "line protocol" in line:
             interface = line.split()[0]
             result[interface] = {}
-        elif 'Internet address' in line:
+        elif "Internet address" in line:
             ip_address = line.split()[-1]
-            result[interface]['ip'] = ip_address
-        elif 'MTU' in line:
+            result[interface]["ip"] = ip_address
+        elif "MTU" in line:
             mtu = line.split()[-2]
-            result[interface]['mtu'] = mtu
+            result[interface]["mtu"] = mtu
 
 print(result)
 """
