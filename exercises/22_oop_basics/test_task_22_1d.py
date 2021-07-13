5dc72bae54d91a4e2bf9bd5d3414f6591a8bc54c
pyneng <pyneng.course@gmail.com>
2021-07-13 12:45:57 +0000

    chapter 22 tests update

--- exercises/22_oop_basics/test_task_22_1d.py
+++ exercises/22_oop_basics/test_task_22_1d.py
@@ -59,7 +59,7 @@ def test_method_add_link(normalized_topology_example, capsys):
         ("R1", "Eth0/4") in return_value.topology
     ), "After adding a connection via the add_link method, it must exist in the topology"
     assert (
-        7 == len(norm_top.topology)
+        7 == len(return_value.topology)
     ), "After adding a connection, the number of connections should be 7"
 
     return_value.add_link(("R1", "Eth0/4"), ("R7", "Eth0/0"))
